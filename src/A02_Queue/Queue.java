61c9a1c9af4fc3cdfb825c5e9e9ed076ed250e0d
Stephan Kochauf, Campus02 <stephan.kochauf@lv.campus02.at>
2024-11-08 16:02:46 +0100

    Implementation von Queue

--- src/A02_Queue/Queue.java
+++ src/A02_Queue/Queue.java
@@ -5,23 +5,35 @@ public class Queue<T>
     private Node<T> first;
     
     private Node<T> last;
+    private int counter;
     /**
      * Das vorderste (=erste) Element aus der Queue entfernen und zurückliefern.
      * Existiert kein Element, wird eine Exception ausgelöst.
      * @throws QueueEmptyException 
      */
     public T dequeue() throws QueueEmptyException {
+        if (counter == 0)
+            throw new QueueEmptyException();
 
-    	return null;
+        Node<T> element = first;
+        first = first.getNext();
+        counter--;
+    	return element.getData();
     }
-    
-    
     
     /**
      * Übergebenen Integer am Ende der Queue anhängen.
      * @param i Zahl
      */
     public void enqueue(T i) {
+        Node<T> newElement = new Node<>(i);
+        counter++;
+        if (first == null)
+            first = newElement;
+        else
+            last.setNext(newElement);
+
+        last = newElement;
 
     }
     
@@ -30,6 +42,6 @@ public class Queue<T>
      * @return
      */
     public int getCount() {
-    	return 0;
+    	return counter;
     }
 }
